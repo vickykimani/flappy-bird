2534b2e049c41848e7e3ab4c2abd6c4907bcc500
vickykimani <victorianightingale70@gmail.com>
2023-05-03 18:04:59 +0300

    base js

---
+++ game.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext("2d");
+
+// we will need the gameCointainer to make it blurry when we display the end menu
+const gameContainer = document.getElementById('game-container');

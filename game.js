2266f2896f49b385b691e6ccc3e3b2b3207c1de0
vickykimani <victorianightingale70@gmail.com>
2023-05-03 18:14:47 +0300

    game constants

--- game.js
+++ game.js
@@ -3,3 +3,13 @@ const ctx = canvas.getContext("2d");
 
 // we will need the gameCointainer to make it blurry when we display the end menu
 const gameContainer = document.getElementById('game-container');
+
+const flappyImg = new Image();
+flappyImg.src = 'assets/flappy_dunk.png';
+
+// game constants
+const FLAP_SPEED = -5;
+const BIRD_WIDTH = 40;
+const BIRD_HEIGHT = 30;
+const PIPE_WIDTH = 50;
+const PIPE_GAP = 125;
